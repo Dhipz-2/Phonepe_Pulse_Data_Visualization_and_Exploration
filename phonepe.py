84591485ad4ef43c9ec93a7fd7c9cd0405e6f11e
Dhipz-2 <73272791+Dhipz-2@users.noreply.github.com>
2023-09-24 23:52:53 +0530

    Add files via upload

---
+++ phonepe.py
@@ -0,0 +1,320 @@
+import os
+import json
+import pandas as pd
+import psycopg2
+
+#AGGREGATED TRANSACTION
+folder_path = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\aggregated\transaction\country\india\state'
+state_list = os.listdir(folder_path)
+
+folder_path2 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\aggregated\transaction\country\india\state\tamil-nadu'
+year_list = os.listdir(folder_path2)
+year_list.remove('2023')
+
+folder_path3 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\aggregated\transaction\country\india\state\tamil-nadu\2018'
+json_list = os.listdir(folder_path3)
+
+d1=[]
+for i in state_list:
+    i1 = os.path.join(folder_path,i)
+    for j in year_list:
+        j1 = os.path.join(i1,j)
+        n=1
+        for k in json_list:
+            k1 = os.path.join(j1,k)
+            f = open(k1)
+            text = json.load(f)
+            length = len(text['data']['transactionData'])
+            for l in range(length):
+                d = dict(TransactionName = text['data']['transactionData'][l]['name'],
+                         NumberOfTransactions = text['data']['transactionData'][l]['paymentInstruments'][0]['count'],
+                         TransactionAmount = text['data']['transactionData'][l]['paymentInstruments'][0]['amount'],
+                         Quater = n,
+                         Year = j,
+                         State = i)
+                d1.append(d)
+            n=n+1
+df1 = pd.DataFrame(d1)
+
+
+#AGGREGATED USER
+folder_path4 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\aggregated\user\country\india\state'
+state_list = os.listdir(folder_path4)
+
+folder_path5 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\aggregated\user\country\india\state\tamil-nadu'
+year_list = os.listdir(folder_path5)
+year_list.remove('2023')
+
+folder_path6 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\aggregated\user\country\india\state\tamil-nadu\2018'
+json_list = os.listdir(folder_path6)
+
+d2=[]
+for i in state_list:
+    i1 = os.path.join(folder_path4,i)
+    for j in year_list:
+        j1 = os.path.join(i1,j)
+        n=1
+        for k in json_list:
+            k1 = os.path.join(j1,k)
+            f = open(k1)
+            text = json.load(f)
+            if text['data']['usersByDevice']!=None:
+                length = len(text['data']['usersByDevice'])
+                for l in range(length):
+                    d = dict(Brand = text['data']['usersByDevice'][l]['brand'],
+                            RegisteredUsers = text['data']['usersByDevice'][l]['count'],
+                            RegisteredUsersPercentage = text['data']['usersByDevice'][l]['percentage'],
+                            Quater = n,
+                            Year = j,
+                            State = i)
+                    d2.append(d)
+            elif text['data']['usersByDevice']==None:
+                d2.append(dict(Brand = None,
+                               RegisteredUsers = text['data']['aggregated']['registeredUsers'],
+                               RegisteredUsersPercentage = None,
+                               Quater = n,
+                               Year = j,
+                               State = i))
+            n=n+1
+df2 = pd.DataFrame(d2)
+#print(df2)
+#print(df2.isnull().sum())
+df2 = df2.dropna()
+
+
+#MAP TRANSACTION
+folder_path7 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\map\transaction\hover\country\india\state'
+state_list = os.listdir(folder_path7)
+
+folder_path8 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\map\transaction\hover\country\india\state\tamil-nadu'
+year_list = os.listdir(folder_path8)
+year_list.remove('2023')
+
+folder_path9 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\map\transaction\hover\country\india\state\tamil-nadu\2018'
+json_list = os.listdir(folder_path9)
+
+d3=[]
+for i in state_list:
+    i1 = os.path.join(folder_path7,i)
+    for j in year_list:
+        j1 = os.path.join(i1,j)
+        n=1
+        for k in json_list:
+            k1 = os.path.join(j1,k)
+            f = open(k1)
+            text = json.load(f)
+            length = len(text['data']['hoverDataList'])
+            for l in range(length):
+                d = dict(District = text['data']['hoverDataList'][l]['name'],
+                         NumberOfTransactions = text['data']['hoverDataList'][l]['metric'][0]['count'],
+                         TransactionAmount = text['data']['hoverDataList'][l]['metric'][0]['amount'],
+                         Quater = n,
+                         Year = j,
+                         State = i)
+                d3.append(d)
+            n=n+1
+
+df3 = pd.DataFrame(d3)
+#print(df3)
+
+
+#MAP USER
+folder_path10 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\map\user\hover\country\india\state'
+state_list = os.listdir(folder_path10)
+
+folder_path11 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\map\user\hover\country\india\state\tamil-nadu'
+year_list = os.listdir(folder_path11)
+year_list.remove('2023')
+
+folder_path12 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\map\user\hover\country\india\state\tamil-nadu\2018'
+json_list = os.listdir(folder_path12)
+
+d4=[]
+for i in state_list:
+    i1 = os.path.join(folder_path10,i)
+    for j in year_list:
+        j1 = os.path.join(i1,j)
+        n=1
+        for k in json_list:
+            k1 = os.path.join(j1,k)
+            f = open(k1)
+            text = json.load(f)
+            length = len(text['data']['hoverData'])
+            for l in text['data']['hoverData'].keys():
+                d = dict(District = l,
+                         RegisteredUser = text['data']['hoverData'][l]['registeredUsers'],
+                         Quater = n,
+                         Year = j,
+                         State = i)
+                d4.append(d)
+            n=n+1
+
+df4 = pd.DataFrame(d4)
+#print(df4)
+
+
+#TOP TRANSACTION
+folder_path13 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\top\transaction\country\india\state'
+state_list = os.listdir(folder_path13)
+
+folder_path14 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\top\transaction\country\india\state\tamil-nadu'
+year_list = os.listdir(folder_path14)
+year_list.remove('2023')
+
+folder_path15 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\top\transaction\country\india\state\tamil-nadu\2018'
+json_list = os.listdir(folder_path15)
+
+d5=[]
+for i in state_list:
+    i1 = os.path.join(folder_path13,i)
+    for j in year_list:
+        j1 = os.path.join(i1,j)
+        n=1
+        for k in json_list:
+            k1 = os.path.join(j1,k)
+            f = open(k1)
+            text = json.load(f)
+            length = len(text['data']['districts'])
+            for l in range(length):
+                d = dict(TopDistrict = text['data']['districts'][l]['entityName'],
+                         TotalTransaction = text['data']['districts'][l]['metric']['count'],
+                         TotalValue = text['data']['districts'][l]['metric']['amount'],
+                         Quater = n,
+                         Year = j,
+                         State = i)
+                d5.append(d)
+            n=n+1
+
+df5 = pd.DataFrame(d5)
+#print(df5)
+
+
+#TOP USER
+folder_path16 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\top\user\country\india\state'
+state_list = os.listdir(folder_path16)
+
+folder_path17 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\top\user\country\india\state\tamil-nadu'
+year_list = os.listdir(folder_path17)
+year_list.remove('2023')
+
+folder_path18 = r'C:\Users\Dhipika\OneDrive\Desktop\streamlit\pulse\data\top\user\country\india\state\tamil-nadu\2018'
+json_list = os.listdir(folder_path18)
+
+d6=[]
+for i in state_list:
+    i1 = os.path.join(folder_path16,i)
+    for j in year_list:
+        j1 = os.path.join(i1,j)
+        n=1
+        for k in json_list:
+            k1 = os.path.join(j1,k)
+            f = open(k1)
+            text = json.load(f)
+            length = len(text['data']['districts'])
+            for l in range(length):
+                d = dict(TopDistrict = text['data']['districts'][l]['name'],
+                         TotalRegisteredUsers = text['data']['districts'][l]['registeredUsers'],
+                         Quater = n,
+                         Year = j,
+                         State = i)
+                d6.append(d)
+            n=n+1
+
+df6 = pd.DataFrame(d6)
+#print(df6)
+
+df1 = df1.values.tolist()
+df2 = df2.values.tolist()
+df3 = df3.values.tolist()
+df4 = df4.values.tolist()
+df5 = df5.values.tolist()
+df6 = df6.values.tolist()
+
+hostname = 'localhost'
+database = 'phonepe_db'
+username = 'postgres'
+pwd = 'Dhip@1999'
+port_id = 5432
+
+conn = psycopg2.connect(
+    host = hostname,
+    dbname = database,
+    user = username,
+    password = pwd,
+    port = port_id
+)
+
+cur = conn.cursor()
+
+try:
+    create_script1 = '''CREATE TABLE IF NOT EXISTS transaction_type(
+    Transaction_name varchar(40),
+    Number_of_transactions int,
+    Transaction_amount float,
+    Quater int,
+    Year int,
+    State varchar(40)
+    )'''
+    cur.execute(create_script1)
+    cur.executemany('INSERT INTO transaction_type VALUES (%s, %s, %s, %s, %s, %s)', df1)
+
+    create_script2 = '''CREATE TABLE IF NOT EXISTS register_phone_users(
+    Brand varchar(30),
+    Registered_users int,
+    Users_percentage float,
+    Quater int,
+    Year int,
+    State varchar(40)
+    )'''
+    cur.execute(create_script2)
+    cur.executemany('INSERT INTO register_phone_users VALUES (%s, %s, %s, %s, %s, %s)', df2)
+
+    create_script3 = '''CREATE TABLE IF NOT EXISTS statelevel_transaction(
+    District varchar(40),
+    Number_of_transactions int,
+    Transaction_amount float,
+    Quater int,
+    Year int,
+    State varchar(40)
+    )'''
+    cur.execute(create_script3)
+    cur.executemany('INSERT INTO statelevel_transaction VALUES (%s, %s, %s, %s, %s, %s)', df3)
+
+    create_script4 = '''CREATE TABLE IF NOT EXISTS statelevel_users(
+    District varchar(40),
+    Registered_user int,
+    Quater int,
+    Year int,
+    State varchar(40)
+    )'''
+    cur.execute(create_script4)
+    cur.executemany('INSERT INTO statelevel_users VALUES (%s, %s, %s, %s, %s)', df4)
+
+    create_script5 = '''CREATE TABLE IF NOT EXISTS top_transactions(
+    Top_district varchar(40),
+    Total_number_of_transactions int,
+    Total_transaction_amount float,
+    Quater int,
+    Year int,
+    State varchar(40)
+    )'''
+    cur.execute(create_script5)
+    cur.executemany('INSERT INTO top_transactions VALUES (%s, %s, %s, %s, %s, %s)', df5)
+
+    create_script6 = '''CREATE TABLE IF NOT EXISTS top_users(
+    Top_district varchar(40),
+    Top_registered_users int,
+    Quater int,
+    Year int,
+    State varchar(40)
+    )'''
+    cur.execute(create_script6)
+    cur.executemany('INSERT INTO top_users VALUES (%s, %s, %s, %s, %s)', df6)
+
+except Exception as error:
+            print(error)
+
+conn.commit()
+conn.close()
+
+
---
+++ phonepe_dashboard.py
@@ -0,0 +1,173 @@
+import streamlit as st
+import pandas as pd
+import psycopg2
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+
+hostname = 'localhost'
+database = 'phonepe_db'
+username = 'postgres'
+pwd = ''
+port_id = 5432
+
+conn = psycopg2.connect(
+    host = hostname,
+    dbname = database,
+    user = username,
+    password = pwd,
+    port = port_id
+)
+
+cur = conn.cursor()
+
+#DASHBOARD
+
+base="light"
+primaryColor="#252224"
+secondaryBackgroundColor="#f3d7ee"
+textColor="#542f71"
+font="serif"
+
+
+st.set_page_config(layout="wide")
+st.header("PHONEPE PULSE DATA VISUALIZATION AND EXPLORATION",divider='grey')
+
+box = st.sidebar.selectbox(
+    "**TRANSACTION OR USER**",
+    ('Transaction', 'User')
+)
+
+year = st.sidebar.selectbox(
+    "**Select Year**",
+    ('2018','2019','2020','2021','2022')
+)
+
+quarter = st.sidebar.radio(
+    "**Select Quarter**",
+    ("Q1", "Q2", "Q3", "Q4")
+)
+
+
+tab1, tab2, tab3 = st.tabs(["**Payment transaction and Users Device**".center(20,"\u2001"), 
+                                  "**Statewise Transactions**".center(40,"\u2001"), 
+                                  "**Top States**".center(10,"\u2001")])
+
+
+if quarter=='Q1':
+    quarter = 1
+elif quarter=='Q2':
+    quarter = 2
+elif quarter=='Q3':
+    quarter = 3
+else:
+    quarter = 4
+with tab1:
+    
+    if box=='Transaction':
+        payment_options = st.selectbox("**Select Transaction type**",
+                                    ("Recharge & bill payments", "Peer-to-peer payments", "Merchant payments", "Financial Services","Others"))
+        try:
+            cur.execute(f"select transaction_name, number_of_transactions, transaction_amount, state from transaction_type where year ={year} and quater={quarter} and transaction_name ='{payment_options}';")
+            sql = cur.fetchall()
+            df = pd.DataFrame(sql, columns=['Transaction_type','Total_transactions', 'Total_amount','State']).reset_index(drop=True)
+            fig = px.bar(df, x='State', y='Total_transactions',
+                        hover_data=['Transaction_type'], color='Total_amount',
+                        color_continuous_scale='Agsunset', width = 1000, height=600)
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+            cur.execute(f"select transaction_name, sum(number_of_transactions) from transaction_type where year ={year} and quater={quarter} group by transaction_name;")
+            sql1 = cur.fetchall()
+            df1 = pd.DataFrame(sql1, columns=['Transaction_Type','Total_transactions']).reset_index(drop=True)
+            st.table(df1)
+
+        except Exception as error:
+            print(error)
+
+    elif box=='User':
+        try:
+            cur.execute(f"select brand, registered_users, users_percentage, state from register_phone_users where year ={year} and quater={quarter};")
+            sql = cur.fetchall()
+            df = pd.DataFrame(sql, columns=['Users_by_device','Registered_users', 'Users_percentage','State']).reset_index(drop=True)
+            fig = px.treemap(df, path=[px.Constant("India"), 'State', 'Users_by_device'], values='Registered_users',
+                color='Registered_users', hover_data=['Users_percentage'],
+                color_continuous_scale='Agsunset')
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+            cur.execute(f"select brand, sum(registered_users) from register_phone_users where year ={year} and quater={quarter} group by brand;")
+            sql1 = cur.fetchall()
+            df1 = pd.DataFrame(sql1, columns=['Transaction_Type','Total_transactions']).reset_index(drop=True)
+            st.table(df1)
+
+        except Exception as error:
+            print(error)
+
+with tab2:
+    if box=='Transaction':
+        st.subheader("Statewise Transactions")
+        cur.execute(f'select state, sum(number_of_transactions), sum(transaction_amount) from statelevel_transaction where year = {year} and quater = {quarter} group by state;')
+        sql = cur.fetchall()
+        df = pd.DataFrame(sql, columns=['State','Total_transactions','Total_amount'])
+        df2 = pd.read_csv(r"C:\Users\Dhipika\Downloads\Statenames.csv")
+        df.State = df2
+        
+        fig = px.choropleth(df, locations='State', geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
+                            featureidkey='properties.ST_NM',color_continuous_scale='Agsunset',
+                            color='Total_transactions', hover_data=['Total_amount'])
+        fig.update_geos(fitbounds='locations', visible=False)
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+        cur.execute(f"select brand, sum(registered_users) from register_phone_users where year ={year} and quater={quarter} group by brand;")
+        sql1 = cur.fetchall()
+        df1 = pd.DataFrame(sql1, columns=['Transaction_Type','Total_transactions']).reset_index(drop=True)
+        st.table(df1)
+
+    elif box=='User':
+        st.subheader("Statewise Users")
+        cur.execute(f'select state, sum(registered_user) from statelevel_users where year = {year} and quater = {quarter} group by state;')
+        sql = cur.fetchall()
+        df = pd.DataFrame(sql, columns=['State','Total_Users'])
+        df2 = pd.read_csv(r"C:\Users\Dhipika\Downloads\Statenames.csv")
+        df.State = df2
+        
+        fig = px.choropleth(df, locations='State', geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
+                            featureidkey='properties.ST_NM',color_continuous_scale='Agsunset',
+                            color='Total_Users')
+        fig.update_geos(fitbounds='locations', visible=False)
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+with tab3:
+
+    if box=='Transaction':
+        st.subheader("Top 10 State Transactions")
+        cur.execute(f'select state, sum(total_number_of_transactions), sum(total_transaction_amount) from top_transactions where year = {year} and quater = {quarter} group by state order by sum(total_number_of_transactions) desc limit 10;')
+        sql = cur.fetchall()
+        df = pd.DataFrame(sql, columns=['Top_State','Top_transactions', 'Total_amount'])
+
+        fig=px.pie(df,names='Top_State', values='Top_transactions', hole = 0.4, color='Total_amount',
+                   hover_data='Total_amount',color_discrete_sequence=px.colors.sequential.Agsunset, labels={'Top_State':'Top States'})
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+        st.table(df.iloc[:,:2])
+        
+    elif box=='User':
+        st.subheader("Top 10 State Users")
+        cur.execute(f'select state, sum(top_registered_users) from top_users where year = {year} and quater = {quarter} group by state order by sum(top_registered_users) desc limit 10;')
+        sql = cur.fetchall()
+        df = pd.DataFrame(sql, columns=['Top_State_Users','Top_Registered_Users'])
+
+        fig=px.pie(df,names='Top_State_Users', values='Top_Registered_Users', hole = 0.4,
+                   color_discrete_sequence=px.colors.sequential.Agsunset, labels={'Top_State_Users':'Top States Users'})
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        
+        st.table(df)
+
+conn.commit()
+conn.close()
+    
+
+    
+
+    
